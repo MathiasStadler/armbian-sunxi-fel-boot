37f3187b5194c92a02ff9251f7f3ecd4dcba44f3
MathiasStadler <stadler-mathias@web.de>
2017-09-21 13:05:49 +0200

    save

--- run.sh
+++ run.sh
@@ -10,22 +10,31 @@ vagrant plugin install vagrant-vbguest
 
 #armbian vagrant version
 echo "clone armbian vagrant version"
-git clone https://github.com/igorpecovnik/lib.git armbian-vagrant
+git clone https://github.com/armbian/build armbian-vagrant
 
 echo "change to armbian-vagrant"
-cd armbian-vagrant
+#cd $(dirname armbian-vagrant)
+pushd armbian-vagrant
 
 echo "save orginal Vagrantfile save"
-cp Vargentfile Vagrantfile_save
+cp Vagrantfile Vagrantfile_save
 
 echo "replace Vagrantfile"
 cp ../Vagrantfile Vagrantfile
+
+echo "vagrant box update => we will work with the last version"
+vagrant box update
 
 echo "vagrant up"
 echo "the next thning make the provisioning_script inside the Vagrantfile"
 vagrant up
 
+vagrant status
+#popd
 
+
+echo "Login inside Box type"
+echo "vagrant ssh"
 
 
 
